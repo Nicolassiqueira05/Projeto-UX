f2b513134ff0bce68cf130e4705343a4576f5b50
Nicolas Siqueira <109821803+Nicolassiqueira05@users.noreply.github.com>
2023-11-19 02:37:10 -0300

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -15,11 +15,14 @@ function close(){
 function autoOpen(){  
     navbar.style.display = "flex"
     openBtn.style.display = "none"
-    closeBtn.style.diplay = "none"
+    closeBtn.style.display = "none"
 }
 function autoClose() {
     navbar.style.diplay = "none"
+    openBtn.style.display = "flex"
+    closeBtn.style.display = "flex"
 }
+
 openBtn.addEventListener("click", open, false)
 closeBtn.addEventListener("click", close, false)
-addEventListener("resize", (event) =>{event.target.innerWidth >= 1500 ? autoOpen() : null}, false)
+addEventListener("resize", (event) =>{event.target.innerWidth >= 1500 ? autoOpen() : autoClose()}, false)
---
+++ pedidos.js
@@ -0,0 +1,36 @@
+let list = document.querySelector("#cart-list")
+let screen = document.querySelector("#content-main")
+const baseScreen = screen.innerHTML
+let cartList = []
+
+function addOrRemove(product){
+    let found = false;
+    for (let i = 0; i < cartList.length; i++) {
+        if (product === cartList[i]) {
+            cartList.splice(i, 1)
+            found = true
+            console.log("removed")
+            break
+        }
+    }
+    if (!found){
+        cartList.push(product)
+    }
+}
+function printList() {
+    list.innerHTML = null
+    for(let i = 0; i < cartList.length; i++){
+        list.innerHTML += `<li class="list-item">${cartList[i]}</li>`
+    }
+}
+function handleAdd(product){
+    addOrRemove(product)
+    printList()
+}
+function handleCancel(){
+    cartList = []
+    printList()
+}
+function handleConfirm(){
+    screen.innerHTML = "</div><div id='checked'><img id='checked-icon' src='img/Checked.png'><h1 id='checked-text'>Pedido realizado com sucesso</h1></div>"
+}
